02fee296c871abaa27ecec50aee9f655d9c108bb
sunzhengyu <sunzhengyu@tuzhanai.com>
2019-09-02 20:06:06 +0800

    init: 优秀代码片段

---
+++ src/main/java/com/szy/code/snippet/file/LoadClassPathFile.java
@@ -0,0 +1,19 @@
+package com.szy.code.snippet.file;
+
+import java.util.Properties;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author sunzhengyu
+ * @create 2019/9/2
+ * @since 1.0.0
+ */
+public class LoadClassPathFile {
+
+
+    public static Properties readClassPathPropertiesFile(String fileName){
+
+    }
+}
