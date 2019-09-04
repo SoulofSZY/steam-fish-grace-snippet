8104c4d58d9bb62f5fcbd2d04744510e006a0445
sunzhengyu <sunzhengyu@tuzhanai.com>
2019-09-04 19:24:47 +0800

    feature: 加载classpath下字典文件

--- src/main/java/com/szy/code/snippet/file/LoadClassPathFile.java
+++ src/main/java/com/szy/code/snippet/file/LoadClassPathFile.java
@@ -1,10 +1,12 @@
 package com.szy.code.snippet.file;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 /**
  * 〈一句话功能简述〉<br>
- * 〈〉
+ * 〈从classpath 加载配置的一般实现〉
  *
  * @author sunzhengyu
  * @create 2019/9/2
@@ -12,8 +14,17 @@ import java.util.Properties;
  */
 public class LoadClassPathFile {
 
+    public static Properties readClassPathPropertiesFile(String fileName) throws IOException{
+        Properties properties = new Properties();
+        try (BufferedInputStream bis = new BufferedInputStream(LoadClassPathFile.class.getResourceAsStream(fileName))) {
+            properties.load(bis);
+        }
+        return properties;
+    }
 
-    public static Properties readClassPathPropertiesFile(String fileName){
+    public static void main(String[] args) throws IOException {
+        Properties properties = readClassPathPropertiesFile("/app.properties");
 
+        System.out.println(properties.getProperty("name"));
     }
 }
